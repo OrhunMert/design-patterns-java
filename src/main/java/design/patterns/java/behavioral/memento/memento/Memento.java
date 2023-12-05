d22e0e8b3dfa4dd8428aea7e0dcdb2ba98bddfed
OrhunMert <70032540+OrhunMert@users.noreply.github.com>
2023-12-05 20:49:20 +0300

    Memento Design Pattern

---
+++ src/main/java/design/patterns/java/behavioral/memento/memento/Memento.java
@@ -0,0 +1,14 @@
+package design.patterns.java.behavioral.memento.memento;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
