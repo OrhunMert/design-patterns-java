d212af0d63dbb2374af060f262f27575805d5dad
OrhunMert <70032540+OrhunMert@users.noreply.github.com>
2023-12-11 23:09:41 +0300

    Strategy Design Pattern

---
+++ src/main/java/design/patterns/java/behavioral/strategy/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package design.patterns.java.behavioral.strategy.strategy;
+
+public interface Strategy {
+    int calculate(int number1, int number2);
+}
---
+++ src/main/java/design/patterns/java/behavioral/strategy/strategy/impl/AddStrategy.java
@@ -0,0 +1,11 @@
+package design.patterns.java.behavioral.strategy.strategy.impl;
+
+import design.patterns.java.behavioral.strategy.strategy.Strategy;
+
+public class AddStrategy implements Strategy {
+
+    @Override
+    public int calculate(int number1, int number2) {
+        return number1 + number2;
+    }
+}
