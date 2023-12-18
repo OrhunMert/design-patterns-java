7080a5d160f6bead7d361b626256e6d5532082a2
OrhunMert <70032540+OrhunMert@users.noreply.github.com>
2023-12-18 23:00:25 +0300

    Template Method Design Pattern

---
+++ src/main/java/design/patterns/java/behavioral/templatemethod/GlassHouse.java
@@ -0,0 +1,14 @@
+package design.patterns.java.behavioral.templatemethod;
+
+public class GlassHouse extends HouseTemplate {
+
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Glass Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with glass coating");
+    }
+}
